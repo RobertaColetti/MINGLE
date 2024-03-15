53db8c60bc5dbb9359f0ff87fae294b72d0e2923
RobertaColetti <128734515+RobertaColetti@users.noreply.github.com>
2024-03-15 10:33:59 +0100

    Add files via upload

---
+++ 2_Creation-starting-methylomics-dataset.R
@@ -0,0 +1,59 @@
+#This script creates the starting methylation dataset for each glioma type by associating the exclusive pairs of genes (genes linked by relations that are exclusive for a glioma type) 
+
+#load the desired JGL outcome:
+load("~/Network-discovery-results/JGL-lam109-lam20001.RData")
+
+#separate the three networks (one per each glioma type):
+Theta_a=fgl.results$theta[[1]]
+Theta_o=fgl.results$theta[[2]]
+Theta_g=fgl.results$theta[[3]]
+
+Theta_a=Theta_a-diag(dim(Theta_a)[1])*Theta_a
+Theta_o=Theta_o-diag(dim(Theta_o)[1])*Theta_o
+Theta_g=Theta_g-diag(dim(Theta_g)[1])*Theta_g
+
+#put 1 if there is a link between two variables
+Theta_a[Theta_a!=0] <-1
+Theta_o[Theta_o!=0] <-1
+Theta_g[Theta_g!=0] <-1
+
+#detect the exclusive relations removing the ones in Astro and Oligo
+Ex_GBM= Theta_g - Theta_a - Theta_o
+Ex_Astro= Theta_a - Theta_g - Theta_o
+Ex_Oligo= Theta_o - Theta_a - Theta_g
+#This operation lead to matrices with only 3 possible values:
+# 0 --> No edge in all glioma types OR shared edge with one other type (e.g., Ex_GBM = Theta_g - Theta_a - Theta_o = 0 - 0 - 0 OR 1 - 1 - 0)
+# 1 --> Exclusive edge (the edge only exists in the first matrix: e.g., Ex_GBM = Theta_g - Theta_a - Theta_o = 1 - 0 - 0)
+#-1 --> Exclusive edge in another glioma type (e.g., Ex_GBM = Theta_g - Theta_a - Theta_o = 0 - 1 - 0)
+#-2 --> Shared edge in the two other glioma types (e.g., Ex_GBM = Theta_g - Theta_a - Theta_o = 0 - 1 - 1)
+
+#Since we are interested in EXCLUSIVE edge, we only want to consider the pairs associated to 1:
+ex_pairs_GBM=colnames(Ex_GBM)[which(Ex_GBM == 1, arr.ind=T)[, "col"]]
+ex_pairs_GBM=ex_pairs_GBM[!duplicated(ex_pairs_GBM)]
+ex_pairs_Astro=colnames(Ex_Astro)[which(Ex_Astro == 1, arr.ind=T)[, "col"]]
+ex_pairs_Astro=ex_pairs_Astro[!duplicated(ex_pairs_Astro)]
+ex_pairs_Oligo=colnames(Ex_Oligo)[which(Ex_Oligo == 1, arr.ind=T)[, "col"]]
+ex_pairs_Oligo=ex_pairs_Oligo[!duplicated(ex_pairs_Oligo)]
+
+#association of the genes with CpG sites 
+load("~/Multi-omics codes/map-genes-probes-1_1.RData")
+starting_probes_GBM=CpG2Gene$CpG[which(CpG2Gene$Gene %in% ex_pairs_GBM)]
+starting_probes_Astro=CpG2Gene$CpG[which(CpG2Gene$Gene %in% ex_pairs_Astro)]
+starting_probes_Oligo=CpG2Gene$CpG[which(CpG2Gene$Gene %in% ex_pairs_Oligo)]
+
+#creating the methylomics starting dataset ----- 
+#load the methylomics dataset (samples classified according to 2021-WHO)
+load("~/Multi-omics codes/dataset/DNAMeth-completeDS.RData")
+#load the RNASeq dataset (to match samples)
+load("~/Multi-omics codes/dataset/common-normally-distributed-RNAseq-2021class.RData")
+
+gbm_samples=rownames(GBM_RNA)
+oligo_samples=rownames(oligo_RNA)
+astro_samples=rownames(astro_RNA)
+
+#Create the initial dataset for each glioma type by matching samples (accross omics) and variables (exclusive pairs)
+astro_DNAMeth=astro_met[which(rownames(astro_met) %in% astro_samples),which(colnames(astro_met) %in% starting_probes_Astro)]
+oligo_DNAMeth=oligo_met[which(rownames(oligo_met) %in% oligo_samples),which(colnames(oligo_met) %in% starting_probes_Oligo)]
+GBM_DNAMeth=gbm_met[which(rownames(gbm_met) %in% gbm_samples),which(colnames(gbm_met) %in% starting_probes_GBM)]
+
+#save(astro_DNAMeth,oligo_DNAMeth,GBM_DNAMeth, file= " DNAMeth_data-ExEdge_RNAseq.RData") #uncomment if you want to save your outcomes
---
+++ 4_MINGLE.R
@@ -0,0 +1,135 @@
+# title: ------ Create MINGLE Networks -------
+# author: Roberta Coletti
+
+# This script includes MINGLE function 
+#(ref.: Coletti et al., A Novel Tool for Multi-Omics Network Integration and Visualization: A Study of Glioma Heterogeneity 2024)
+
+# MINGLE function inputs:
+# net: the underlying network used as reference to compute the edges (methylomics networks in Coletti et al.). Nodes of this network will be referred as "Nodes of underlying layer" (NodesUL)
+# map: the functional link between the NodesUL and the new MINGLE Nodes (map between CpG and genes in Coletti et al.)
+# edge.type: label indenfitying the desired function to compute the edge weights. There are two possibilities:
+# 
+# 1) strength: edges are computed by the median value of the weights of the links of all the probes related to a given gene;
+#
+# 2) count: edges are computed by counting the number of connection of all the probes related to a given gene. It can be weighted or not. 
+#           If "count" is selected, MINGLE needs as input a list of weights which dimension must match with the number of nodes of the MINGLE network.
+# In Coletti et al. the MINGLE networks are constituted by the genes associated to the selected CpGs, and the weights the percentages of selected probes.
+# 
+
+
+MINGLE <- function(net, map, edge.type,weights){
+
+  #Save the Node of the underlying network
+  NodesUL=colnames(net)
+  #Save the MINGLE nodes
+  M.nodes=map[which(map[,1] %in% NodesUL),2]
+  M.nodes=M.nodes[!duplicated(M.nodes)]
+  
+  #check the input are correct:
+  if(edge.type != "strength" AND edge.type !="count")
+  {  print("Error! Invalid edge.type.")
+    return()
+    #geterrmessage()
+  }
+  if (edge.type=="count")
+  {
+    if (length(weights)!=length(M.nodes))
+    {  print("Error! Count measure has been selected; weights must be set.")
+      return()
+      #geterrmessage()
+    }
+  }
+  
+  #compute the MINGLE networks
+  MINGLE.net=matrix(0, length(M.nodes),length(M.nodes))
+  rownames(MINGLE.net)=colnames(MINGLE.net)=M.nodes
+  last=length(M.nodes)-1
+  for (i in c(1:last) )
+  { first=i+1 #Only the upper triangular matrix is created
+  for (j in c(first:length(M.nodes))) 
+    #identification of the NodesUL linked to the i-th MINGLE node 
+  { #rows of the NodesUL linked with i-th MINGLE node
+    rows_NodesUL.i=which(map[,2] %in% M.nodes[i])
+    #find the indexes of the adjacency matrix corresponding to the NodesUL associated to the i-th MINGLE node
+    index_i=which(NodesUL %in% map[rows_NodesUL.i,1])
+    #same with j-th MINGLE node
+    rows_NodesUL.j=which(map[,2] %in% M.nodes[j])
+    index_j=which(NodesUL %in% map[rows_NodesUL.j,1])
+    
+    vectorized_net=net[index_i,index_j]
+    vectorized_net=vectorized_net[vectorized_net!=0]
+    if (edge.type =="count")
+    {
+      w=(weights[i]+weights[j])/2
+      MINGLE.net[i,j]=length(vectorized_net)*w
+    }
+    if(edge.type =="strength")
+    { 
+      MINGLE.net[i,j]=median(abs(vectorized_net))
+      MINGLE.net[is.na(MINGLE.net)] <- 0
+    }
+  }
+  }
+  MINGLE.net=MINGLE.net+t(MINGLE.net)
+ return(MINGLE.net) 
+}
+
+
+#----- library section
+library("qgraph")
+library("base")
+
+#script to call the function:
+
+#1) Load data:
+#methylomics glasso results (related to GBM):
+load("~/Multi-omics codes/glasso-results/results_glasso-rho085-meth-gbm.RData") 
+
+#map CpG --> Genes
+load("~/Multi-omics codes/map-genes-probes-1_1.RData")
+
+#reduce the map to the starting GBM dataset:
+red.CpG2Gene=CpG2Gene[which(CpG2Gene$CpG %in% colnames(gbm_met_i)),]
+associated_genes=red.CpG2Gene$Gene[which(red.CpG2Gene$CpG %in% selected_meth)]
+associated_genes=associated_genes[!duplicated(associated_genes)]
+
+# ---- if Count measure is selected ---- 
+# Weight computing:
+# determining the percentage of selected probes for each gene 
+percentage_sel_probes=matrix(0,nrow = length(associated_genes), ncol = 2) 
+rownames(percentage_sel_probes)=associated_genes
+colnames(percentage_sel_probes)=c("percentage","Tot_probes")
+for(j in c(1:length(associated_genes))) #for each gene...
+{  #count the total number of CpG sites 
+  index=which(red.CpG2Gene$Gene %in% associated_genes[j]) 
+  Total= length(index)
+  #count the number of the selected probes 
+  Selected= length(which(colnames(Sel_Theta)%in% red.CpG2Gene$CpG[index]))
+  percentage_sel_probes[j,1]=Selected/Total
+  percentage_sel_probes[j,2]=Total
+}
+
+
+MINGLE.net=MINGLE(Sel_Theta,red.CpG2Gene,edge.type = "strength",1)
+#MINGLE.net=MINGLE(Sel_Theta,red.CpG2Gene,edge.type = "count", percentage_sel_probes[,1])
+
+#Graphical part:
+#Remove isolated nodes for graphical representation (i.e. genes that are selected for self-related probe relations)
+MINGLE.net=MINGLE.net[apply(MINGLE.net, 2, var, na.rm=TRUE) != 0,apply(MINGLE.net, 2, var, na.rm=TRUE) != 0]
+MINGLE.nodes=colnames(MINGLE.net)
+
+qgraph(MINGLE.net, layout = "spring", 
+       directed = FALSE, edge.labels = F, 
+       esize = 8,
+       labels=MINGLE.nodes,
+       label.font=12,
+       label.prop=1,
+       legend = F,
+       border.color = "black",border.width = 2,
+       edge.width = 0.8,
+       label.color = "black" )
+title(sub = "Strength MINGLE network") #if edge.type = "strength"
+#title(sub = "Count MINGLE network") #if edge.type = "count"
+
+
+
